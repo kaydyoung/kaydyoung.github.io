56eedd10f5c1d87ce0ec42fc6b33837be8da5c57
kaydyoung <kdyoung96@gmail.com>
2021-01-22 13:46:36 -0600

    slight dark mode tweak

--- _site/assets/js/darkmodetoggle.js
+++ _site/assets/js/darkmodetoggle.js
@@ -3,4 +3,4 @@ const btn = document.querySelector(".btn-toggle");
 // Listen for a click on the button
 btn.addEventListener("click", function() {
   // Then toggle (add/remove) the .dark-theme class to the body
-  document.body.classList.toggle("dark-theme");  });
+  document.body.classList.toggle(".dark-theme");  }); // Added a period before the theme ?? Maybe thats what it needed
