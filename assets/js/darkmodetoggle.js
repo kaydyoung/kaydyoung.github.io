1d4e6cc1e5901baad95501f239ff8bcddfae7733
kaydyoung <kdyoung96@gmail.com>
2021-01-22 18:56:23 -0600

    darkmodeupdates
    implementing dark mode fully, just need to update the colors and set preferred theme in memory

--- assets/js/darkmodetoggle.js
+++ assets/js/darkmodetoggle.js
@@ -1,6 +1,18 @@
-const btn = document.querySelector(".btn-toggle");
+function toggleDark() {
+  var mode = "";
+  //if user toggles to dark mode, select dark class
+  if (document.body.classList.toggle("dark")){
+    mode = "Light Mode"
+  } else {
+    mode = "Dark Mode"
+    }
+      //Mode will be the text inside the button: dark or light
+      document.getElementById("btn-toggle").innerHTML = mode;
+    }
 
-// Listen for a click on the button
-btn.addEventListener("click", function() {
-  // Then toggle (add/remove) the .dark-theme class to the body
-  document.body.classList.toggle(".dark-theme");  }); // Added a period before the theme ?? Maybe thats what it needed
+//Saving preferences
+if (mode = "Light Mode") {
+  localStorage.setItem("preffered-mode", "light");
+} else {
+  localStorage.setItem("preffered-mode", "dark")
+  }
