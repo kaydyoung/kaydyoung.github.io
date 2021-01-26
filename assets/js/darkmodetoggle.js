e84b8b6f22e559c9c5e35237a79a5d9337d0a18a
kaydyoung <kdyoung96@gmail.com>
2021-01-25 22:28:38 -0600

    lots dark mode updates
    added a toggle that doesn't work

--- assets/js/darkmodetoggle.js
+++ assets/js/darkmodetoggle.js
@@ -10,9 +10,39 @@ function toggleDark() {
       document.getElementById("btn-toggle").innerHTML = mode;
     }
 
-//Saving preferences
-if (mode = "Light Mode") {
-  localStorage.setItem("preffered-mode", "light");
-} else {
-  localStorage.setItem("preffered-mode", "dark")
-  }
+
+/* Saving Preferences
+
+const theme = document.getElementById("btn-toggle").innerHTML
+
+window.localStorage.setItem("pref", theme);
+
+window.localStorage.getItem("pref")
+*/
+
+document.getElementsByClassName('.btn__background').onClick(function() {
+  document.getElementsByClassName('.container').toggleClass('color');
+  document.getElementsByClassName('.btn__background').toggleClass('color');
+  document.getElementsByClassName('.btn__face').toggleClass('move');
+    document.getElementsByClassName('.btn__animation').toggleClass(function(){
+      return $(this).is('.move-reverse, .move') ? 'move-reverse move' : 'move';
+    })
+});
+
+/*
+$('.btn__background').on('click', function() {
+  $('.container').toggleClass('color');
+  $('.btn__background').toggleClass('color');
+  $('.sun-box').toggleClass('move');
+  $('.moon-box').toggleClass('move');
+
+  $('.btn__face').toggleClass('move');
+  $('.btn__eye-left, .btn__eye-right').toggleClass(function(){
+    return $(this).is('.open, .close') ? 'open close' : 'close';
+  });
+  $('.btn__mouth').toggleClass('close');
+
+  $('.btn__animation').toggleClass(function(){
+    return $(this).is('.move-reverse, .move') ? 'move-reverse move' : 'move';
+  })});
+*/
