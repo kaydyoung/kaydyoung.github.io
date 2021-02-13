af1a20a7fcdcb21e4dc709d22761ae53093c7aa1
kaydyoung <kdyoung96@gmail.com>
2021-02-12 19:08:13 -0600

    dark mode img working

--- _site/assets/js/darkmodetoggle.js
+++ _site/assets/js/darkmodetoggle.js
@@ -18,18 +18,21 @@ function toggleDark() {
   var img = document.getElementById("modeimage");
   //if user toggles to dark mode, select dark class
   if (document.body.classList.toggle("dark")){
-    img.src("assets/images/moon.png")
+    img.src = "assets/images/sun.png"
+    console.log(img);
   } else {
-    img.src("assets/images/sun.png");
+    img.src = "assets/images/moon.png";
+    console.log(img);
     }
 }
 
 
-/* Saving Preferences
+/* Saving Preferences */
 
-const theme = document.getElementById("btn-toggle").innerHTML
+const theme = document.getElementById("btn-toggle").innerHTML;
 
 window.localStorage.setItem("pref", theme);
 
-window.localStorage.getItem("pref")
-*/
+console.log(theme);
+
+//window.localStorage.getItem("pref");
