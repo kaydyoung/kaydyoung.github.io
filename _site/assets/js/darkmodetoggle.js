108bc7233ed5cbb9f98ff5839b09bb07dd280a11
kaydyoung <kdyoung96@gmail.com>
2021-03-11 12:39:50 -0600

    design updates

--- _site/assets/js/darkmodetoggle.js
+++ _site/assets/js/darkmodetoggle.js
@@ -4,15 +4,34 @@ function toggleDark() {
   //if user toggles to dark mode, select dark class
   if (document.body.classList.toggle("dark")){
     img.src = "assets/images/sun.svg"
-    window.localStorage.setItem('mode', 'dark')
+    window.localStorage.setItem("mode", "dark")
   } else {
     img.src = "assets/images/moon.svg"
-    window.localStorage.setItem('mode', 'light')
+    window.localStorage.setItem("mode", "light")
     }
-  
-    /*
-  var icon = document.getElementsByClassName("social-icons");
-  for each a in "social-icons"
-  Image.src = 
-  */
+  //return window.localStorage.getItem("mode");
 }
+
+
+// Get the current theme from local storage 
+var currentTheme = localStorage.getItem("mode");
+console.log(currentTheme);
+
+if (currentTheme === "dark") {
+  document.body.classList.toggle("dark")
+}
+
+/*
+// If the current local storage item can be found
+if (currentTheme) {
+  (document.body.classList.toggle(currentTheme))
+}
+  currentTheme = localStorage.setItem("mode", "dark")
+  // Set the body data-theme attribute to match the local storage item
+  document.body.classList.toggle(currentTheme);
+// If the current theme is dark, check the theme toggle
+  if (currentTheme == "dark") {
+    document.body.classList.toggle("dark");
+  }
+}
+*/
